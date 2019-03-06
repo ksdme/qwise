6143c89dcb68203e2cb42724de7571f7a2269449
Kilari Teja <kilariteja9@gmail.com>
2019-03-06 20:47:48 +0530

    App.js: Add sample tag buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
 import { QuestionProvider } from './providers/Questions';
+import { InlineTagButton } from './components/InlineTagButton/InlineTagButton';
+import { InlineTagButtonSet } from './components/InlineTagButton/InlineTagButtonSet';
 
 
 class App extends Component {
@@ -10,6 +12,13 @@ class App extends Component {
         <div className="App">
           <p>Edit <code>src/App.js</code> and save to reload.</p>
         </div>
+
+        <InlineTagButtonSet>
+          <InlineTagButton>Hello!</InlineTagButton>
+          <InlineTagButton>THis!</InlineTagButton>
+          <InlineTagButton>Is!</InlineTagButton>
+          <InlineTagButton>Me!</InlineTagButton>
+        </InlineTagButtonSet>
       </QuestionProvider>
     );
   }
