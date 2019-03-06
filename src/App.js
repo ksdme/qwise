bf37f179c3184a98204e4c5091b28d30675c90c8
Kilari Teja <kilariteja9@gmail.com>
2019-03-06 14:06:25 +0530

    App.js: Clean and add questions providers

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
 
+import { QuestionProvider } from './providers/Questions';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <QuestionProvider>
+        <div className="App">
+          <p>Edit <code>src/App.js</code> and save to reload.</p>
+        </div>
+      </QuestionProvider>
     );
   }
 }
