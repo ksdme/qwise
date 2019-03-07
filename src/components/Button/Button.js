0d79eaac631dc2a288a9363bdc5b1e6bd94696b1
Kilari Teja <kilariteja9@gmail.com>
2019-03-07 13:26:30 +0530

    Button.js: Add danger color to button

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,12 +3,13 @@ import React from 'react';
 import { react } from '../../dollar';
 
 
-export const Button = ({ children, outline, info, primary, onClick, className, }) => {
+export const Button = ({ children, outline, info, primary, danger, onClick, className, }) => {
 
   const classes = react.ifClasses({
     'button': true,
     'has-margin': true,
     'is-info': info,
+    'is-danger': danger,
     'is-primary': primary,
     'is-outlined': outline,
     className: !!className,
