32384b8541c0deb7663253fa1dfca73f70e0ab12
Kilari Teja <kilariteja9@gmail.com>
2019-03-07 22:29:12 +0530

    Questions.js: Add simpler Questions provider

--- src/providers/Questions.js
+++ src/providers/Questions.js
@@ -1,8 +1,7 @@
 import React from 'react';
 
-import { react } from '../dollar';
 import { QuestionSource } from '../sources/questions';
 
 
 export const QuestionContext = React.createContext(QuestionSource);
-export const QuestionProvider = react.repacked(QuestionContext.Provider);
+export const QuestionProvider = QuestionContext.Provider;
