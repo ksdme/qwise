bd68dc95811a30343055d3c80b143d18e8997d1c
Kilari Teja <kilariteja9@gmail.com>
2019-03-07 20:53:47 +0530

    Question: Add support for custom classnames

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
+import { react } from '../../dollar';
 
-export const Question = ({ url, alt }) => {
+
+export const Question = ({ className, url, alt }) => {
+
+  const classes = react.ifClasses({
+    _: [[className, !!className]],
+  });
 
   return (
-    <div className="container">
-      <img src={url} alt={alt} />
+    <div className={`level ${classes}`}>
+      <div className="level-item">
+        <img className="image" src={url} alt={alt} />
+      </div>
     </div>
   );
 
