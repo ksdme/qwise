8522b40fa7b19c369666f6cac6c9e26ae222fed1
Kilari Teja <kilariteja9@gmail.com>
2019-03-07 14:01:38 +0530

    InlineTagButton: Convert to class based Component and add highlight toggling

--- src/components/InlineTagButton/InlineTagButton.js
+++ src/components/InlineTagButton/InlineTagButton.js
@@ -4,18 +4,39 @@ import styled from 'styled-components';
 import { react } from '../../dollar';
 
 
-export const InlineTagButtonBordered = ({ children, className }) => {
-  const classes = react.ifClasses({
-    tag: true,
-    button: true,
-    _: [[className, !!className]],
-  });
+export class InlineTagButtonBordered extends React.Component {
 
-  return (
-    <span className={classes}>
-      { children }
-    </span>
-  );
+  constructor() {
+    super();
+
+    this.state = {
+      clicked: false,
+    };
+  }
+
+  onTagClick() {
+    this.setState({
+      clicked: !this.state.clicked,
+    });
+  }
+
+  render() {
+    const { children, className } = this.props;
+
+    const classes = react.ifClasses({
+      tag: true,
+      button: true,
+      'is-success': this.state.clicked,
+      _: [[className, !!className]],
+    });
+
+    return (
+      <span className={classes} onClick={this.onTagClick.bind(this)}>
+        { children }
+      </span>
+    );
+  }
+
 };
 
 export const InlineTagButton = styled(InlineTagButtonBordered)`
