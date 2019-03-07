f3340c9d5406e89d38d2b37f92708f8b747c949e
Kilari Teja <kilariteja9@gmail.com>
2019-03-07 12:54:49 +0530

    OptionPanel: Add a box around OptionPanel

--- src/components/Options/OptionPanel.js
+++ src/components/Options/OptionPanel.js
@@ -1,18 +1,27 @@
+import './OptionPanel.css';
+
 import React from 'react';
 
 import { OptionButton, SubmitButton } from './OptionButton';
+import { react } from '../../dollar';
 
 
 export class OptionPanel extends React.Component {
 
   render() {
+    const topClasses = react.ifClasses({
+      box: true,
+    });
+
     return (
-      <div className="field is-grouped is-grouped-centered">
-        <OptionButton>A</OptionButton>
-        <OptionButton>B</OptionButton>
-        <OptionButton>C</OptionButton>
-        <OptionButton>D</OptionButton>
-        <SubmitButton>Submit</SubmitButton>
+      <div className={topClasses}>
+        <div className="field is-grouped is-grouped-centered">
+          <OptionButton>A</OptionButton>
+          <OptionButton>B</OptionButton>
+          <OptionButton>C</OptionButton>
+          <OptionButton>D</OptionButton>
+          <SubmitButton>Submit</SubmitButton>
+        </div>
       </div>
     );
   }
