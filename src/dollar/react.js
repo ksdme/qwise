23af8964be5b603f33d5c6d0529fde9121830960
Kilari Teja <kilariteja9@gmail.com>
2019-03-07 12:27:48 +0530

    react.js: Add conditional classes

--- src/dollar/react.js
+++ src/dollar/react.js
@@ -11,7 +11,21 @@ const klass = (klass) => {
   return klass ? klass : '';
 };
 
+const ifClass = (klass, conditional) => {
+  return conditional ? klass : '';
+};
+
+const ifClasses = (map) => {
+  const classes = Object.keys(map).filter((key) => {
+    return map[key] === true;
+  });
+
+  return classes.join(' ');
+};
+
 export default {
   repacked,
   klass,
+  ifClass,
+  ifClasses,
 };
