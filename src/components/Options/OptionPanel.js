f1e00dadea83958a7bef5b53d70a1db1b6b8bbd2
Kilari Teja <kilariteja9@gmail.com>
2019-03-07 13:27:33 +0530

    OptionPanel: Add left and right content to OptionPanel

--- src/components/Options/OptionPanel.js
+++ src/components/Options/OptionPanel.js
@@ -11,16 +11,19 @@ export class OptionPanel extends React.Component {
   render() {
     const topClasses = react.ifClasses({
       box: true,
+      fixed: this.props.fixed,
     });
 
     return (
       <div className={topClasses}>
         <div className="field is-grouped is-grouped-centered">
+          { this.props.leftContent }
           <OptionButton>A</OptionButton>
           <OptionButton>B</OptionButton>
           <OptionButton>C</OptionButton>
           <OptionButton>D</OptionButton>
           <SubmitButton>Submit</SubmitButton>
+          { this.props.rightContent }
         </div>
       </div>
     );
