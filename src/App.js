a269ae4de3c9a4bd13b1d7e741acfd2ef25e2c31
Kilari Teja <kilariteja9@gmail.com>
2019-03-08 17:53:05 +0530

    App.js: Update

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import { OptionPanel } from './components/Options/OptionPanel';
 import { Control } from './components/Control/Control';
 import { ArrowButton } from './components/Actions/ArrowButton';
 import { Message } from './components/Message/Message';
+import { random } from './dollar';
 
 
 class App extends Component {
@@ -17,47 +18,81 @@ class App extends Component {
   constructor() {
     super();
 
-    this.state = {
+    this.pseudo = {
+      tags: [],
       list: [],
-      current: {
-        a: "A",
-        tags: ["Math", "Medium", "Algebra"],
-        q: "http://localhost:5000/Selection_216.bmp",
-        status: null,
-      },
+      rand: null,
+    };
+
+    this.state = {
+      // holds question
     };
 
     // Questions Source
     this.questions = QuestionSource;
+    this.pseudo.rand = this.getSuitableRand();
+  }
+
+  getSuitableRand() {
+    const max = this.pseudo.list.length - 1;
+    return random.nonRecurringRandIntGenerator(0, max);
+  }
+
+  syncQuestions() {
+    const tags = this.pseudo.tags;
+    const list = this.questions.getStrictTagged(...tags);
+
+    this.pseudo.list = list;
+    this.pseudo.rand = this.getSuitableRand();
+  }
+
+  loadSomeQuestion() {
+    const local = this.pseudo.rand();
+
+    if (local === undefined)
+      this.setState({
+        q: null,
+        a: null,
+        status: null,
+      });
+
+    else
+      this.setState({
+        ...this.pseudo.list[local],
+        status: null,
+        selected: null,
+      });
   }
 
   onSubmitAnswer(selected) {
-    const status = selected === this.state.current.a;
+    const status = selected === this.state.a;
 
     this.setState({
-      current: {
-        ...this.state.current,
-        status,
-      },
+      ...this.state,
+      selected,
+      status,
     });
   }
 
   answerSelectedToComponent() {
-    if (this.state.current.status !== null) {
-      if (this.state.current.status === true) {
+    if (this.state.status !== null) {
+      if (this.state.status === true) {
         return (
           <Message primary>
             <span role="img" aria-label="Yay!">🎉</span> Correct Answer <br/>
-            <span className="tag is-light">Good Work!</span>
+            <span className="tag is-light">Correct Option is { this.state.a }</span>
           </Message>
         );
       }
 
-      if (this.state.current.status === false) {
+      if (this.state.status === false) {
         return (
           <Message danger>
             <span role="img" aria-label="Wrong">😞</span> Wrong Answer <br/>
-            <span className="tag is-light">Correct Option is { this.state.current.a }</span>
+            <span className="tag is-light">
+              Correct Option is { this.state.a } <br/>
+              Your answer was { this.state.selected }
+            </span>
           </Message>
         );
       }
@@ -66,24 +101,47 @@ class App extends Component {
     return null;
   }
 
+  onTagSelectionUpdate(tags) {
+    this.pseudo.tags = tags;
+    this.syncQuestions();
+  }
+
+  componentWillMount() {
+    if (this.pseudo.list.length === 0) {
+      this.syncQuestions();
+      this.loadSomeQuestion();
+    }
+  }
+
   render() {
+    const nextQuestion = (
+      <Control>
+        <ArrowButton
+          onClick={this.loadSomeQuestion.bind(this)}
+          direction='right' />
+      </Control>
+    );
+
     return (
       <Section>
         <div className="container is-centered">
           <AppTags
             tags={this.questions.getAvailableTags()}
-            onTagSetChanged={(l) => console.log(l)} />
+            onTagSetChanged={this.onTagSelectionUpdate.bind(this)} />
 
           <Question
             className="has-xl-padding"
-            url={this.state.current.q} />
+            url={this.state.q} />
 
-          <OptionPanel fixed
-            onSubmit={this.onSubmitAnswer.bind(this)}
-            leftContent={<Control><ArrowButton direction='left' /></Control>}
-            rightContent={<Control><ArrowButton direction='right' /></Control>}
-            bottomContent={this.answerSelectedToComponent()}
-          />
+          {
+            (this.state.a) ?
+              <OptionPanel fixed
+                onSubmit={this.onSubmitAnswer.bind(this)}
+                rightContent={nextQuestion}
+                bottomContent={this.answerSelectedToComponent()}
+              /> :
+              null
+          }
         </div>
       </Section>
     );
