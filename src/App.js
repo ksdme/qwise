58985c8f16b489c3306ba2e1b628a45cac7819ba
Kilari Teja <kilariteja9@gmail.com>
2019-03-08 11:45:18 +0530

    App.js: Add sample question and checking

--- src/App.js
+++ src/App.js
@@ -14,6 +14,55 @@ import { Message } from './components/Message/Message';
 
 
 class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      current: {
+        a: "A",
+        tags: ["Math", "Medium", "Algebra"],
+        q: "http://localhost:5000/Selection_216.bmp",
+        status: null,
+      },
+    };
+  }
+
+  onSubmitAnswer(selected) {
+    const status = selected === this.state.current.a;
+
+    this.setState({
+      current: {
+        ...this.state.current,
+        status,
+      },
+    });
+  }
+
+  answerSelectedToComponent() {
+    if (this.state.current.status !== null) {
+      if (this.state.current.status === true) {
+        return (
+          <Message primary>
+            Correct Answer <br/>
+            Good Work!
+          </Message>
+        );
+      }
+
+      if (this.state.current.status === false) {
+        return (
+          <Message danger>
+            Wrong Answer <br/>
+            Correct Option is { this.state.current.a }
+          </Message>
+        );
+      }
+    }
+
+    return null;
+  }
+
   render() {
     return (
       <QuestionProvider value={QuestionSource}>
@@ -23,13 +72,13 @@ class App extends Component {
 
             <Question
               className="has-xl-padding"
-              url='http://localhost:5000/Selection_216.bmp' />
+              url={this.state.current.q} />
 
             <OptionPanel fixed
-              onSubmit={() => alert(123)}
+              onSubmit={this.onSubmitAnswer.bind(this)}
               leftContent={<Control><ArrowButton direction='left' /></Control>}
               rightContent={<Control><ArrowButton direction='right' /></Control>}
-              bottomContent={<Message danger>Wrong Answer<br/>Hello</Message>}
+              bottomContent={this.answerSelectedToComponent()}
             />
           </div>
         </Section>
